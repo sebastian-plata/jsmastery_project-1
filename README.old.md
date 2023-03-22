2d6bb85e6dadffd96f90d0461590df97d8a75696
sebastian-plata <113690137+sebastian-plata@users.noreply.github.com>
2023-03-22 21:06:14 +0100

    build: prepare basic file structure for project

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# jsmastery_projects
+
+First project of the 4 proposed by jsmastery youtube.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Article, Brand, CTA, Feature, Navbar} from "./components"
+
+const App = () => {
+  return (
+    <div>
+      <h1>gpt-3</h1>
+    </div>
+  )
+}
+
+export default App
